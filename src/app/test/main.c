1b1e76381720a43ee47c3e11a066e22dddb552df
Damian <18800429335@163.com>
2024-07-02 08:15:25 +0800

    202407020816

---
+++ src/app/test/main.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include "sys_plat.h"
+#include "echo/tcp_echo_client.h"
+#include "echo/tcp_echo_server.h"
+
+static sys_semt sem;
+static sys_mutex_t mutex;
+static int count
+
+static char buffer[100];
+static int write_index, read_index, total;
+static sys_sem_t read_sem, write_sem;
+
+void thread1_entry(void* arg) {
+	for (int i = 0; i < 2 * sizeof(buffer); ++i) {
+		sys_sem_wait(read_sem, 0);
+
+		char data = buffer[read_index++];
+	}
+}
---
+++ src/net/net/net.h
@@ -0,0 +1,8 @@
+#ifndef NET_H
+#define NET_H
+
+net_err_t net_init(void);
+net_err_t net_start(void);
+
+
+#endif
